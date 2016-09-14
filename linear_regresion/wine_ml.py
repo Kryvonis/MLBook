88703256522dae8738ff6ab33e503b68c3b2e3a9
kryvonis <Kryvonis.artem@gmail.com>
2016-09-14 10:53:34 +0300

    r-square wine prediction is 0.32..0.35

--- linear_regresion/wine_ml.py
+++ linear_regresion/wine_ml.py
@@ -1,10 +1,13 @@
+from sklearn.linear_model import LinearRegression
+from sklearn.cross_validation import train_test_split
 import pandas as pd
 import matplotlib.pyplot as plt
 df = pd.read_csv('../datasets/wine/winequality-red.csv', sep=';')
-print(df.describe())
+X = df[list(df.columns)[:-1]]
+y = df['quality']
+X_train, X_test, y_train, y_test = train_test_split(X,y)
 
-plt.scatter(df['alcohol'], df['quality'])
-plt.xlabel('Alcohol')
-plt.ylabel('Quality')
-plt.title('Alchol against quality')
-plt.show()
+regressor = LinearRegression()
+regressor.fit(X_train,y_train)
+y_prediction = regressor.predict(X_test)
+print('R-quared : {}'.format(regressor.score(X_test,y_test)))
